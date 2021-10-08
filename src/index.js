7f26942b0192be834562a61649a2050133f1eae8
hector <polbarazorda@gmail.com>
2021-10-07 19:18:35 -0500

    fix cors

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ const express = require('express')
 const app = require('./app')
 const notFound = require('./middleware/notFound.js')
 const handleErrors = require('./middleware/handleErrors.js')
-const cors = require('cors')
 
-app.use(cors())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  next()
+})
+
 app.use(express.json())
 app.use(notFound)
 app.use(handleErrors)
